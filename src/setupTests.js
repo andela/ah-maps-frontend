6f26af4c5061c6a79911f7e83275bb2b8ef92ca4
Tony Ndichu <ke-c02ppctafvh3@KE-C02PPCTAFVH3s-MacBook-Pro.local>
2018-11-26 17:05:55 +0300

    chore(testing): setup testing environment
        - install enzyme
        - setup sample test
    
    [Finishes #161255188]

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
 import { configure} from 'enzyme';
+=======
+import { configure,shallow } from 'enzyme';
+>>>>>>> chore(testing): setup testing environment
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+global.shallow=shallow
