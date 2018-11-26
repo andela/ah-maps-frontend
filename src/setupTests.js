7c0fa0dfe38d0812bc03e0a77e1e9dd274b76932
Tony Ndichu <ke-c02ppctafvh3@KE-C02PPCTAFVH3s-MacBook-Pro.local>
2018-11-26 17:05:55 +0300

    chore(testing): setup testing environment
        - install enzyme
        - setup sample test
    
    [Finishes #161255188]

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,6 +5,13 @@ import { configure,shallow } from 'enzyme';
 >>>>>>> chore(testing): setup testing environment
 import Adapter from 'enzyme-adapter-react-16';
 
+<<<<<<< HEAD
 configure({ adapter: new Adapter() });
+<<<<<<< HEAD
 
 global.shallow=shallow
+=======
+=======
+configure({ adapter: new Adapter() });
+>>>>>>> chore(testing): setup testing environment
+>>>>>>> chore(testing): setup testing environment
