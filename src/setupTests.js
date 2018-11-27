87dcbd763a86a64be7fc6c97b28a4982404ac544
Mirr77 <mirrmaina@gmail.com>
2018-11-27 12:34:52 +0300

    chore(testing): setup testing environment     - install enzyme     - setup sample test
    [Finishes #161255188]

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
-import { configure} from 'enzyme';
-=======
-import { configure,shallow } from 'enzyme';
->>>>>>> chore(testing): setup testing environment
+import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-<<<<<<< HEAD
 configure({ adapter: new Adapter() });
-<<<<<<< HEAD
 
-global.shallow=shallow
-=======
-=======
-configure({ adapter: new Adapter() });
->>>>>>> chore(testing): setup testing environment
->>>>>>> chore(testing): setup testing environment
