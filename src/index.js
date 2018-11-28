92e0d24fc9e74b876bb9842f5b594f8ee54d0e41
Paul Kinuthia <pkinuthia10@gmail.com>
2018-11-28 12:33:18 +0300

    chore(setup test): configure jest test coverage (#5)
    - strive to increase test coverage
    - configure jest collect coverage
    
    [Delivers #161255188]

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import './assets/styles/index.css';
+import './assets/styles/index.sass';
 import { store } from './redux/store';
 import App from './views/Landingpage';
 import * as serviceWorker from './serviceWorker';
--- src/redux/actions/__tests__/articles.actions.test.js
+++ src/redux/actions/__tests__/articles.actions.test.js
@@ -1,5 +1,5 @@
 import { ADD_ARTICLES } from '../../constants';
-import { addArticles } from '../articles';
+import { addArticles } from '..';
 
 describe('Article action creators', () => {
   it('Should dispatch ADD_ARTICLE type', () => {
