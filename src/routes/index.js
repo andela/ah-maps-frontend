91d36ca38b20363c4d6a9b18e87a5171777847cc
Tony-Ndichu <37045046+Tony-Ndichu@users.noreply.github.com>
2018-11-29 12:03:14 +0300

    #161255190 Setup react router (#7)
    * chore(routes): Setup react router version 4
    
    - install react router dom
    - setup mapping for routes array
    
    [WIP #161255190]
    
    * [chore #161255190] Add login route
    
    * [chore #161255190]Add login tests
    
    * chore(routes): Setup react router version 4
    
    - install react router dom
    - setup mapping for routes array
    
    [Finishes #161255190]

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,17 @@
 import LandingPage from '../views/Landingpage';
+import Login from '../views/Login';
 
 const routes = [
   {
     path: '/',
     name: 'Landing Page',
     component: LandingPage,
+  },
+
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
   },
 
 ];
