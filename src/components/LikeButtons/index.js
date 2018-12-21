ee0d8ccd4fd5ad9a7839d465ee25d074e020f3df
MartinKaburu <martin.kaburu@andela.com>
2018-12-21 12:27:36 +0300

    bug(like/dislike): Fix the like/dislike icons
    - clicking the label likes article
    - add the thumbs-down icon
    
    [#161255202]

--- src/components/LikeButtons/index.js
+++ src/components/LikeButtons/index.js
@@ -5,20 +5,20 @@ import { isLoggedIn } from '../../utils';
 
 const LikeBtn = ({ ...props }) => {
   const {
-    likeCount, dislikeCount
+    likeCount, dislikeCount,
   } = props;
   return (
-      <div className="customContainer">
-          <i className={isLoggedIn() ? "teal large thumbs up icon onclick-magnify" : "teal disabled large thumbs up icon onclick-magnify"} onClick={props.handleLike} />
-        <a className="ui basic teal left pointing label">
-          { likeCount }
-        </a>
-&nbsp;
-        <i className={isLoggedIn() ? "teal large thumbs up icon onclick-magnify" : "teal disabled large thumbs down icon onclick-magnify"} onClick={props.handleDislike} />
-        <a className="ui basic teal left pointing label">
-          { dislikeCount }
-        </a>
-      </div>
+    <div className="customContainer">
+      <i className={isLoggedIn() ? 'teal large thumbs up icon onclick-magnify' : 'teal disabled large thumbs up icon'} onClick={props.handleLike} />
+      <a className="ui basic teal left pointing label" onClick={props.handleLike}>
+        { likeCount }
+      </a>
+&nbsp;&nbsp;
+      <i className={isLoggedIn() ? 'teal large thumbs down icon onclick-magnify' : 'teal disabled large thumbs down icon'} onClick={props.handleDislike} />
+      <a className="ui basic teal left pointing label" onClick={props.handleDislike}>
+        { dislikeCount }
+      </a>
+    </div>
   );
 };
 
